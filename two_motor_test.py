b1e57df91ec6fbe3e3e0484d443745c9f1cc84a6
ShOlayemi <147769040+ShOlayemi@users.noreply.github.com>
2023-12-05 21:07:27 +0100

    Update two_motor_test.py

--- two_motor_test.py
+++ two_motor_test.py
@@ -1,32 +1,19 @@
-#!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
+from ev3dev2.motor import LargeMotor
+from time import sleep
+from pixycamev3.pixy2 import Pixy2
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-MOTOR_RUNTIME = 5000  # Motor runtime in milliseconds
 MOTOR_SPEED = 500  # Motor speed in degrees per second
 TURN_ANGLE = 180  # Turn angle in degrees
 TURN_ANGLE_UP_DOWN = 90  # Turn angle for up and down motion in degrees
 
-from pixycamev3.pixy2 import Pixy2
+# Connect large motors to different output ports, e.g., output port A, B, and C
+motor_forward = LargeMotor('outA')
+motor_up_down_1 = LargeMotor('outB')
+motor_up_down_2 = LargeMotor('outC')
 
 pixy2 = Pixy2(port=2, i2c_address=0x54)
 
 pixy2.set_lamp(1, 0)
-
-# Connect large motors to different output ports, e.g., output port A, B, and C
-"motor_forward = LargeMotor('outA')
-motor_up_down_1 = LargeMotor('outA')
-motor_up_down_2 = LargeMotor('outB')
-
-# Function to move the robot forward for a specified time
-def move_forward(time_sp):
-    motor_forward.run_timed(time_sp=time_sp, speed_sp=MOTOR_SPEED)
-    motor_forward.wait_until_not_moving()  # Wait until the motor stops
 
 # Function to turn the robot by a specified angle
 def turn_robot(angle):
@@ -42,9 +29,9 @@ def turn_up_down(angle):
     motor_up_down_1.wait_until_not_moving()
     motor_up_down_2.wait_until_not_moving()
 
-while True :
-    detected_objects = pixy2.get_blocks(1,1)
-    print("Distance: {} cm, Detected Objects: {}".format(distance, detected_objects))
+while True:
+    detected_objects = pixy2.get_blocks(1, 1)
+    print("Detected Objects: {}".format(detected_objects))
 
     # Check if the Pixy2 camera detects any objects
     if detected_objects:
@@ -53,4 +40,5 @@ while True :
 
         print("Turning up and down 90 degrees.")
         turn_up_down(TURN_ANGLE_UP_DOWN)  # Turn the robot up and down by 90 degrees
-    sleep(1000)
+
+    sleep(1)  # Sleep for 1 second to avoid excessive loop frequency
