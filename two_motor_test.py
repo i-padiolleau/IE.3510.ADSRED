74e22fb421f95a70d4af80ef0b51046b84b73b42
ShOlayemi <147769040+ShOlayemi@users.noreply.github.com>
2023-12-07 22:35:05 +0100

    Update two_motor_test.py
    MOTOR A

--- two_motor_test.py
+++ two_motor_test.py
@@ -15,7 +15,7 @@ TURN_ANGLE_UP_DOWN = 90  # Turn angle for up and down motion in degrees
 us = UltrasonicSensor(INPUT_1)
 
 # Connect large motors to different output ports, e.g., output port A, B, and C
-#motor_forward = LargeMotor(OUTPUT_A)
+motor_forward = LargeMotor(OUTPUT_A)
 motor_up_down_1 = LargeMotor(OUTPUT_B)
 motor_up_down_2 = LargeMotor(OUTPUT_C)
 
@@ -31,9 +31,10 @@ def measure_distance():
     return us.distance_centimeters
 
 # Function to turn the robot by a specified angle
-#def turn_robot(angle):
-    #motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle)
-    #motor_forward.wait_until_not_moving()  # Wait until the motor stops
+def turn_robot(angle):
+    motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle)
+    sleep(1)
+    motor_forward.wait_until_not_moving()  # Wait until the motor stops
 
 # Function to turn the robot up and down by a specified angle for both motors
 def turn_up_down(angle):
@@ -56,8 +57,8 @@ while True:
 
     # Check if the Pixy2 camera detects any objects
     if detected_objects:
-        #print("Objects detected. Turning 180 degrees.")
-        #turn_robot(TURN_ANGLE)  # Turn the robot 180 degrees
+        print("Objects detected. Turning 180 degrees.")
+        turn_robot(TURN_ANGLE)  # Turn the robot 180 degrees
 
         print("Turning up and down 90 degrees.")
         turn_up_down(TURN_ANGLE_UP_DOWN)  # Turn the robot up and down by 90 degrees
