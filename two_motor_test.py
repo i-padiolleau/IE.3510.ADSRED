752b273ae67ad774c1774b3c4baafe45803bc393
ShOlayemi <147769040+ShOlayemi@users.noreply.github.com>
2023-12-08 20:27:51 +0100

    Update two_motor_test.py

--- two_motor_test.py
+++ two_motor_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1, INPUT_2
 from ev3dev2.sensor.lego import UltrasonicSensor
