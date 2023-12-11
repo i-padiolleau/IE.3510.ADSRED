c107d2137d94e7b30d2ae135c045be6ee03b0ee2
ShOlayemi <147769040+ShOlayemi@users.noreply.github.com>
2023-12-11 15:52:47 +0100

    Update two_motor_test.py

--- two_motor_test.py
+++ two_motor_test.py
@@ -1,15 +1,98 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1, INPUT_2
-from ev3dev2.sensor.lego import UltrasonicSensor
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
+
 from time import sleep
 
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.sound import Sound
+from pixycamev3.pixy2 import Pixy2
+
+spkr = Sound()
+pixy2 = Pixy2(port=2, i2c_address=0x54)
 motor_forward = LargeMotor(OUTPUT_A)
 motor_tilt = MoveTank(OUTPUT_B, OUTPUT_C)
 
+pixy2.set_lamp(1, 0)
+sleep(0.5)
+pixy2.set_lamp(0, 0)
 
-print(motor_forward.position)
-print(motor_forward.position())
-motor_tilt.reset()
+resolution = pixy2.get_resolution()
+MOTOR_SPEED = 10
+
+voice = True
+
+# PID constants (adjust these based on your system and tuning)
+kp = 1.0  # Proportional gain
+ki = 0.1  # Integral gain
+kd = 0.01  # Derivative gain
+
+comptx = 0
+compty = 0
+compt_dist = 0
+compute_dist = False
+bb_box = []
+Align = False
+
+# Initialize PID variables
+prev_error = 0
+integral = 0
+
+while True:
+    nbr, target = pixy2.get_blocks(3, 1)
+
+    if nbr == 1:
+        if voice:
+            spkr.speak("target detected")
+            voice = False
+            Align = True
+        x = target[0].x_center
+        y = target[0].y_center
+        w = target[0].width
+        h = target[0].height
+        print(x, y, w, h)
+
+        if Align:
+            # Calculate error
+            error_x = x - 158  # Assume the target is centered at x-coordinate 158
+            error_y = y - 104  # Assume the target is centered at y-coordinate 104
+
+            # Update integral and derivative terms for x-axis
+            integral_x = integral + error_x
+            derivative_x = error_x - prev_error
+
+            # Update integral and derivative terms for y-axis
+            integral_y = integral + error_y
+            derivative_y = error_y - prev_error
+
+            # Calculate PID output for x-axis
+            pid_output_x = kp * error_x + ki * integral_x + kd * derivative_x
+
+            # Calculate PID output for y-axis
+            pid_output_y = kp * error_y + ki * integral_y + kd * derivative_y
+
+            # Update motor positions based on PID outputs
+            motor_forward.on(SpeedPercent(MOTOR_SPEED - pid_output_x), brake=False)
+            motor_tilt.on(SpeedPercent(MOTOR_SPEED - pid_output_y), brake=False)
+
+            # Save current errors for the next iteration
+            prev_error_x = error_x
+            prev_error_y = error_y
+
+            comptx += 1 if abs(error_x) < 10 else 0
+            compty += 1 if abs(error_y) < 10 else 0
+
+            if comptx >= 3 and compty >= 3:
+                print("ready")
+                Align = False
+                compute_dist = True
+                spkr.speak("Ready to fire")
+
+        if compute_dist:
+            if compt_dist >= 10:
+                average_w, average_h = map(lambda z: sum(z) / len(bb_box), zip(*bb_box))
+                print(average_w, average_h)
+                break
+            else:
+                bb_box.append([w, h])
+                compt_dist += 1
+
+    sleep(0.2)
