e858f9126e17736e4e3ebcadd9c33bca021e4c8f
Wilson92-isep <antoine.wilson12@gmail.com>
2023-12-18 11:04:23 +0100

    sfbvdi

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
@@ -13,7 +13,9 @@ from pixycamev3.pixy2 import Pixy2
 spkr = Sound()
 pixy2 = Pixy2(port=2, i2c_address=0x54)
 motor_forward = LargeMotor(OUTPUT_A)
-motor_tilt = MoveTank(OUTPUT_D, OUTPUT_C)
+starting_A = motor_forward.position
+print(starting_A)
+motor_tilt = MoveTank(OUTPUT_B, OUTPUT_C)
 
 pixy2.set_lamp(1, 0)
 sleep(0.5)
@@ -35,7 +37,7 @@ pos_on_y = False
 test = input("start_scanning press enter")
 while True :
     nbr , target = pixy2.get_blocks(3,1)
-    if nbr == 1 : 
+    if nbr >= 1 : 
         if voice : 
             spkr.speak("target detected") 
             voice = False
