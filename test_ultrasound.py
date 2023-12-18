898f18eb5f4743c6125356a45f27fc7e195719cc
Wilson92-isep <antoine.wilson12@gmail.com>
2023-12-18 15:31:03 +0100

    test tir

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -8,8 +8,9 @@ from ev3dev2.sound import Sound
 
 motor_forward = LargeMotor(OUTPUT_A)
 
+print(motor_forward.position)
+
 motor_forward.on_for_degrees(speed=20, degrees=180)
 motor_forward.wait_until_not_moving()
 
-motor_forward.on_for_degrees(speed=20, degrees=310)
-motor_forward.wait_until_not_moving()
+print(motor_forward.position)
