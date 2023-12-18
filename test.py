996674fa5714eb91d56816b853a4e54029c9cc89
Wilson92-isep <antoine.wilson12@gmail.com>
2023-12-18 14:30:11 +0100

    test tir

--- test.py
+++ test.py
@@ -17,12 +17,19 @@ motor_tilt = LargeMotor( OUTPUT_C)
 
 print(motor_forward.position , motor_tilt.position)
 
+motor_forward_starting_position , motor_tilt_starting_position = motor_forward.position , motor_tilt.position
+
 pixy2.set_lamp(1, 0)
 sleep(0.5)
 pixy2.set_lamp(0, 0)
 
 resolution = pixy2.get_resolution()
-MOTOR_SPEED = 10
+
+def reboot(x, y, motor1, motor2) : 
+    motor1.on_to_position(20, x)
+    motor2.on_to_position(20, y)
+    motor1.wait_until_not_moving() 
+    motor2.wait_until_not_moving() 
 
 voice = True
 
@@ -36,10 +43,15 @@ compute_dist = False
 bb_box = []
 pos_on_x = False
 pos_on_y = False
+detect = False
+compt_loss = 0 
 test = input("start_scanning press enter")
 while True :
     nbr , target = pixy2.get_blocks(1,1)
+    pos_on_x = False
+    pos_on_y = False
     if nbr >= 1 : 
+        detect = True
         if voice : 
             spkr.speak("target detected") 
             voice = False
@@ -49,50 +61,27 @@ while True :
         w = target[0].width
         h = target[0].height
         print(x, y , w, h)
-        # if Align :
-        #     if x < 148 : 
-        #         angle_x = 30 - (x/158 * 30)
-        #         motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle_x* 2.5)
-        #         motor_forward.wait_until_not_moving()
-        #         compt = 0
-        #     elif x > 168 :
-        #         angle_x =  -((x-158)/158 * 30) 
-        #         motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle_x* 2.5)
-        #         motor_forward.wait_until_not_moving()        
-        #         compt = 0
-        #     else : 
-        #         pos_on_x = True
-        #     if y < 94 : 
-        #         angle_y = 20 - (y/104 * 20)
-        #         motor_tilt.on_for_degrees(MOTOR_SPEED,angle_y)
-        #         motor_tilt.wait_until_not_moving()
-        #         compt = 0
-        #     elif y > 114 :
-        #         angle_y =  -((y-104)/104 * 20) 
-        #         motor_tilt.on_for_degrees(MOTOR_SPEED,angle_y)
-        #         motor_tilt.wait_until_not_moving()  
-        #         compt = 0
         if Align :
             if x < 148 : 
-                angle_x =  -((x-158)/158 * 30)
-                motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle_x* 2.5)
+                angle_x = 30 - (x/158 * 30)
+                motor_forward.on_for_degrees(speed=10, degrees=angle_x* 2.5)
                 motor_forward.wait_until_not_moving()
                 compt = 0
             elif x > 168 :
-                angle_x = 30 - (x/158 * 30)
-                motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=angle_x* 2.5)
+                angle_x =  -((x-158)/158 * 30) 
+                motor_forward.on_for_degrees(speed=10, degrees=angle_x* 2.5)
                 motor_forward.wait_until_not_moving()        
                 compt = 0
             else : 
                 pos_on_x = True
             if y < 94 : 
                 angle_y = 20 - (y/104 * 20)
-                motor_tilt.on_for_degrees(MOTOR_SPEED,angle_y)
+                motor_tilt.on_for_degrees(10,angle_y)
                 motor_tilt.wait_until_not_moving()
                 compt = 0
             elif y > 114 :
                 angle_y =  -((y-104)/104 * 20) 
-                motor_tilt.on_for_degrees(MOTOR_SPEED,angle_y)
+                motor_tilt.on_for_degrees(10,angle_y)
                 motor_tilt.wait_until_not_moving()  
                 compt = 0
         
@@ -105,6 +94,7 @@ while True :
                 Align = False
                 compute_dist = True
                 spkr.speak("Ready to fire") 
+                compt = 0 
 
 
         if compute_dist :
@@ -116,22 +106,40 @@ while True :
                 print("distance with x : {}mm".format(distance_on_x))
                 print("distance with y : {}mm".format(distance_on_y))
                 compute_dist = False  
-                Align = True            
+                shoot = True            
             else : 
                 bb_box.append([w,h])
                 compt_dist += 1 
+
+        if shoot : 
+            spkr.speak("fire in the hole !")
+            reboot(motor_forward_starting_position , motor_tilt_starting_position,motor_forward, motor_tilt)
+            i = 0 
+            shoot = False
+            voice = True
+
+    elif detect :
+        if compt_loss >= 3 : 
+            detect = False
+            reboot(motor_forward_starting_position , motor_tilt_starting_position,motor_forward, motor_tilt)
+            i = 0 
+            compt_loss = 0
+            voice = True 
+            Align = False
+        else :
+            compt_loss += 1 
 
     else : 
         move = sequence[i]
         print(i, move)
         if move == 1 : 
-            motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=30 * 2.5)
+            motor_forward.on_for_degrees(speed=15, degrees=30 * 2.5)
         elif move == -1 : 
-            motor_forward.on_for_degrees(speed=MOTOR_SPEED, degrees=-30 * 2.5)
+            motor_forward.on_for_degrees(speed=15, degrees=-30 * 2.5)
         elif move == 2 : 
-            motor_tilt.on_for_degrees(MOTOR_SPEED,27)
+            motor_tilt.on_for_degrees(15,27)
         elif move == -2 : 
-            motor_tilt.on_for_degrees(MOTOR_SPEED,-27)
+            motor_tilt.on_for_degrees(15,-27)
         i += 1 
         if i ==len(sequence) : 
             print(motor_forward.position , motor_tilt.position)
