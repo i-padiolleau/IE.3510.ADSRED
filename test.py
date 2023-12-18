f8f620fa19597c948ca1842d77a63a452af8a964
Wilson92-isep <79054472+Wilson92-isep@users.noreply.github.com>
2023-12-18 14:52:36 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from ev3dev2.sound import Sound
 from pixycamev3.pixy2 import Pixy2
 
 spkr = Sound()
-pixy2 = Pixy2(port=2, i2c_address=0x54)
+pixy2 = Pixy2(port=1, i2c_address=0x54)
 motor_forward = LargeMotor(OUTPUT_A)
 motor_tilt = LargeMotor( OUTPUT_C)
 motor_shoot = MediumMotor(OUTPUT_D)
