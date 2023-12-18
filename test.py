4b0d771540ad79175cc00a2a04b5fc071637fb5f
Wilson92-isep <79054472+Wilson92-isep@users.noreply.github.com>
2023-12-18 11:11:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
