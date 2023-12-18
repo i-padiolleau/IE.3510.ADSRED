b4067acb47ac90e1b3284ae163a1418ad3881395
Wilson92-isep <79054472+Wilson92-isep@users.noreply.github.com>
2023-12-18 14:33:19 +0100

    Update test_ultrasound.py

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -1,12 +1,12 @@
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
-motor_forward = LargeMotor(OUTPUT_D)
+motor_forward = LargeMotor(OUTPUT_B)
 
 motor_forward.on_for_degrees(speed=40, degrees=270)
 motor_forward.wait_until_not_moving()
